31778569b8b66beaa98dd1a5aae9264ad52f1312
chiru9693 <chiruchiru24939@gmail.com>
2026-03-28 19:02:41 +0530

    Update processor.py

--- processor.py
+++ processor.py
@@ -1,15 +1,18 @@
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 from sentiment import analyze
 from database import insert
 import time
 import os
 
+# 🔥 IMPORTANT: function must be outside
+def process(text):
+    score, sentiment, _, _ = analyze(text)
+    return (text, score, sentiment)
+
+
 def process_texts(texts):
 
-    def process(text):
-        score, sentiment, _, _ = analyze(text)
-        return (text, score, sentiment)
-
+    # ================= NORMAL =================
     start_normal = time.time()
 
     normal_results = []
@@ -18,14 +21,16 @@ def process_texts(texts):
 
     normal_time = round(time.time() - start_normal, 2)
 
+    # ================= PARALLEL =================
     start_parallel = time.time()
 
-    with ThreadPoolExecutor(max_workers=5) as executor:
+    # 🔥 REAL PARALLEL (MULTIPROCESSING)
+    with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
         parallel_results = list(executor.map(process, texts))
 
     parallel_time = round(time.time() - start_parallel, 2)
 
-    # 🔥 INSERT AFTER PROCESSING (NOT INSIDE LOOP)
+    # ================= INSERT =================
     for text, score, sentiment in parallel_results:
         insert(text, score, sentiment)
 
